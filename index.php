036e17299e580a85c4d05e9c76ff84c3f8fd52ae
micodian <micodian83@gmail.com>
2021-03-04 19:44:02 -0600

    initial index page

--- index.php
+++ index.php
@@ -4,9 +4,44 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <title>Document</title>
+    <link rel="stylesheet" href="view/css/main.css">
 </head>
 <body>
-    <h1>Test Page</h1>
+    <div class="container-fluid">
+        <header><h1>Zippy Used Autos</h1></header>
+
+        <div class="container">
+        <table class="table table-striped table-dark">
+            <thead class="thead-dark">
+                <tr>
+                <th scope="col">Year</th>
+                <th scope="col">Make</th>
+                <th scope="col">Model</th>
+                <th scope="col">Type</th>
+                <th scope="col">Class</th>
+                <th scope="col">Price</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                <th scope="row">1</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+                </tr>
+            </tbody>
+        </table>
+        </div>
+        <footer>
+        <p>&copy; <?php echo date("Y"); ?> Zippy Used Autos</p>
+        </footer>
+    </div>
+    
+    
 </body>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 </html>
