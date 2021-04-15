8d596e636b035c68c1ffd4a36cde7c3f9b5d5cc7
micodian <micodian83@gmail.com>
2021-04-15 10:09:38 -0500

    fixed jawsdb issue

--- model/database.php
+++ model/database.php
@@ -4,20 +4,9 @@ class Database{
     // private static $dsn= 'mysql:host=localhost;dbname=zippyusedautos';
     // private static $username= 'root';
     // private static $password='';
-    // private static $db;
+    
 
-    // private static $url;
-    // private static $dbparts;
-    // private static $hostname;
-    // private static $username;
-    // private static $password ;
-    // private static $database ;
-
-    // // private static $dbname;
-    // // private static $host;
-    // // private static $username;
-    // // private static $password;
-    // private static $dsn;
+    
     private static $db;
 
 
@@ -31,42 +20,49 @@ class Database{
     public static function getDB(){
         
         if(!isset(self::$db)){
+            if(getenv('JAWSDB_URL',$local_only=false))
+            {
 
 
+                    try {
+                        $url = getenv('JAWSDB_URL');
+                        $dbparts = parse_url($url);
 
-            try {
-                $url = getenv('JAWSDB_URL');
-                $dbparts = parse_url($url);
+                        $hostname = $dbparts['host'];
+                        $username = $dbparts['user'];
+                        $password = $dbparts['pass'];
+                        $database = ltrim($dbparts['path'],'/');
+                        self::$db = new PDO("mysql:host=$hostname;dbname=$database", $username, $password);
+                        // set the PDO error mode to exception
+                        self::$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                        //echo "Connected successfully";
+                        }
+                    catch(PDOException $e)
+                        {
+                        echo "Connection failed: " . $e->getMessage();
+                        }
 
-                $hostname = $dbparts['host'];
-                $username = $dbparts['user'];
-                $password = $dbparts['pass'];
-                $database = ltrim($dbparts['path'],'/');
-                self::$db = new PDO("mysql:host=$hostname;dbname=$database", $username, $password);
-                // set the PDO error mode to exception
-                self::$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-                //echo "Connected successfully";
+                    }            
+            else{       
+                try {
+                    $dsn= 'mysql:host=localhost;dbname=zippyusedautos';
+                    $username= 'root';
+                    $password='';
+                    self::$db = new PDO($dsn,$username,$password);
+                    //echo 'connection good';
+                } catch (PDOException $e) {
+                    $error =$e->getMessage();
+                    include('view/error.php');
+                    exit();
                 }
-            catch(PDOException $e)
-                {
-                echo "Connection failed: " . $e->getMessage();
-                }
-
-            // try {
-            //     self::$db = new PDO(self::$dsn, self::$username, self::$password);
-            //     //echo 'connection good';
-            // } catch (PDOException $e) {
-            //     $error =$e->getMessage();
-            //     include('view/error.php');
-            //     exit();
-            // }
+            }
         }
         return self::$db;
     }
 
    
 }
-//$database = new Database();
+
    
 
 
