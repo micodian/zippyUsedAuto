a31379773d9563ce6534f6b989fb42f59dfa0c95
micodian <micodian83@gmail.com>
2021-04-15 09:44:56 -0500

    test

--- model/database.php
+++ model/database.php
@@ -9,15 +9,15 @@ class Database{
     // private static $url;
     // private static $dbparts;
     // private static $hostname;
-    private static $username;
-    private static $password ;
+    // private static $username;
+    // private static $password ;
     // private static $database ;
 
     // // private static $dbname;
     // // private static $host;
     // // private static $username;
     // // private static $password;
-    private static $dsn;
+    // private static $dsn;
     private static $db;
 
 
@@ -26,27 +26,40 @@ class Database{
 
 
     private function __construct(){
-        $url = getenv('JAWSDB_URL');
-        $dbparts = parse_url($url);
-    
-        $hostname = $dbparts['host'];
-        self::$username = $dbparts['user'];
-        self::$password = $dbparts['pass'];
-        $database = ltrim($dbparts['path'],'/');
-        self::$dsn = "mysql:host=$hostname;dbname=$database";
+        
     }
     public static function getDB(){
         
         if(!isset(self::$db)){
 
+
+
             try {
-                self::$db = new PDO(self::$dsn, self::$username, self::$password);
-                //echo 'connection good';
-            } catch (PDOException $e) {
-                $error =$e->getMessage();
-                include('view/error.php');
-                exit();
-            }
+                $url = getenv('JAWSDB_URL');
+                $dbparts = parse_url($url);
+
+                $hostname = $dbparts['host'];
+                $username = $dbparts['user'];
+                $password = $dbparts['pass'];
+                $database = ltrim($dbparts['path'],'/');
+                self::$db = new PDO("mysql:host=$hostname;dbname=$database", $username, $password);
+                // set the PDO error mode to exception
+                self::$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                //echo "Connected successfully";
+                }
+            catch(PDOException $e)
+                {
+                echo "Connection failed: " . $e->getMessage();
+                }
+
+            // try {
+            //     self::$db = new PDO(self::$dsn, self::$username, self::$password);
+            //     //echo 'connection good';
+            // } catch (PDOException $e) {
+            //     $error =$e->getMessage();
+            //     include('view/error.php');
+            //     exit();
+            // }
         }
         return self::$db;
     }
@@ -59,13 +72,13 @@ class Database{
 
 
 
-    $url = getenv('JAWSDB_URL');
-    $dbparts = parse_url($url);
+    // $url = getenv('JAWSDB_URL');
+    // $dbparts = parse_url($url);
 
-    $hostname = $dbparts['host'];
-    $username = $dbparts['user'];
-    $password = $dbparts['pass'];
-    $database = ltrim($dbparts['path'],'/');
+    // $hostname = $dbparts['host'];
+    // $username = $dbparts['user'];
+    // $password = $dbparts['pass'];
+    // $database = ltrim($dbparts['path'],'/');
 
 
 
